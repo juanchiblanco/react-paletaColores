328f6e1ba35f01c0a2f082aac599f05fe10c1fc2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-20 20:42:06 -0300

    agregue footer, h1 e importe bootstrap para el estilo. Y baje footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
 
   return (
     <>
-      
+    <main className="container my-4">
+      <h1 className="text-center">Paleta de Colores</h1>
+    </main>
+      <footer className="bg-dark text-light text-center py-3">
+        <p>&copy;Todos los derechos reservados</p>
+      </footer>
     </>
   )
 }
